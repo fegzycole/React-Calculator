ae0fb1814046a10d2144cb8e9aac96cd2733c6a3
fegzycole <fergusoniyara@gmail.com>
2020-04-10 09:44:39 +0100

    Make some changes to the calculate.js file

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,53 +1,50 @@
-import operateModule from './operate';
+import operate from './operate';
 
-const { operate } = operateModule;
+const calculate = (calculator, buttonName) => {
+  const operations = ['+', '-', 'X', '÷'];
 
-export default (() => {
-  const calculate = (calculator, buttonName) => {
-    const operations = ['+', '-', 'X', '÷'];
+  let { total, next, operation } = calculator;
 
-    let { total, next, operation } = calculator;
-
-    if (buttonName === '+/-') {
-      if (next) {
-        next = (+next * -1);
-      }
-      total = (+total * -1);
+  if (buttonName === '+/-') {
+    if (next) {
+      next = (+next * -1);
     }
+    total = (+total * -1);
+  }
 
-    if (buttonName === '%') {
-      if (next) {
-        next *= 0.01;
-      } else {
-        total *= 0.01;
-      }
+  if (buttonName === '%') {
+    if (next) {
+      next *= 0.01;
+    } else {
+      total *= 0.01;
     }
+  }
 
-    if (buttonName === '=') {
-      if (next) {
-        total = operate(total, next, operation);
-        next = null;
-      }
-
-      operation = buttonName;
-    }
-
-    if (operations.includes(buttonName)) {
-      if (total && next) {
-        total = operate(total, next, buttonName);
-        next = null;
-        operation = null;
-      }
-    }
-
-    if (buttonName === 'AC') {
-      total = null;
+  if (buttonName === '=') {
+    if (next) {
+      total = operate(total, next, operation);
       next = null;
       operation = null;
     }
-  };
+  }
 
-  return {
-    calculate,
-  };
-})();
+  if (operations.includes(buttonName)) {
+    operation = buttonName;
+  }
+
+  if (buttonName === '.') {
+    if (next && !next.toString().contains('.')) {
+      next = `${next.toString()}.`;
+    }
+  }
+
+  if (buttonName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  }
+
+  return { total, next, operation };
+};
+
+export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,26 +1,22 @@
 import Big from 'big.js';
 
-export default (() => {
-  const operate = (numberOne, numberTwo, operation) => {
-    const x = new Big(numberOne);
-    const y = new Big(numberTwo);
+const operate = (numberOne, numberTwo, operation) => {
+  const x = new Big(numberOne);
+  const y = new Big(numberTwo);
 
-    if (operation === '-') {
-      return x.minus(y);
-    }
-    if (operation === '+') {
-      return x.plus(y);
-    }
-    if (operation === 'X') {
-      return x.mul(y);
-    }
-    if (operation === '÷') {
-      return x.div(y);
-    }
-    return 0;
-  };
+  if (operation === '-') {
+    return x.minus(y);
+  }
 
-  return {
-    operate,
-  };
-})();
+  if (operation === '+') {
+    return x.plus(y);
+  }
+
+  if (operation === 'X') {
+    return x.mul(y);
+  }
+
+  return x.div(y);
+};
+
+export default operate;
